8f55d598358c02f0ad3888fb77e7833538d5cd2b
akashmaverick <akashmistry77@gmail.com>
2025-07-13 14:37:55 +0530

    Your commit message here

---
+++ about.js
@@ -0,0 +1,67 @@
+AOS.init({ once: false });
+
+document.getElementById("hamburgerBtn").onclick = () => {
+  document.getElementById("mobileMenu").classList.add("active");
+};
+document.getElementById("closeMenu").onclick = () => {
+  document.getElementById("mobileMenu").classList.remove("active");
+};
+
+function selectLocation(name) {
+  document.getElementById("locationText").textContent = name;
+}
+
+$(".mobile-sidebar .btn-login").on("click", function () {
+  $("#mobileMenu").removeClass("active");
+});
+
+// Counter Animation
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+  const speed = 200;
+
+  const animateCounter = (counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  };
+
+  const observer = new IntersectionObserver(
+    (entries, observerRef) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          animateCounter(entry.target);
+          observerRef.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 1 }
+  );
+
+  counters.forEach((counter) => observer.observe(counter));
+});
+
+// location
+
+const locationSwiper = new Swiper(".locations-swiper", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: "auto",
+  speed: 3000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  freeMode: true,
+  grabCursor: true,
+});
