29cdbc9b3217405a481d07f0582474fa84a66764
akashmaverick <akashmistry77@gmail.com>
2025-07-13 17:02:04 +0530

    Your commit message here

---
+++ sparePart.js
@@ -0,0 +1,17 @@
+AOS.init({ once: false });
+
+document.getElementById("hamburgerBtn").onclick = () => {
+  document.getElementById("mobileMenu").classList.add("active");
+};
+document.getElementById("closeMenu").onclick = () => {
+  document.getElementById("mobileMenu").classList.remove("active");
+};
+
+function selectLocation(name) {
+  document.getElementById("locationText").textContent = name;
+}
+
+$(".mobile-sidebar .btn-login").on("click", function () {
+  $("#mobileMenu").removeClass("active");
+});
+
